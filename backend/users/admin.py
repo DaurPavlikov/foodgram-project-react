48e9250fd1e5a5beb778ed209bf652470ab73ba6
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-03 12:42:45 +0300

    edit user admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
-from users.models import User
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Subscribe, User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'username',
         'email',
-        'role',
-        'bio',
         'first_name',
         'last_name',
     )
-    search_fields = ('username', 'role',)
-    list_filter = ('username', 'role',)
+    search_fields = ('username', 'email',)
+    list_filter = ('username', 'email',)
     empty_value_display = '-пусто-'
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
