2b8b55dbcb7470fd18db640d8c621d8094cdbb1f
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-03 13:10:38 +0300

    set csv import

--- backend/csv_import_script.py
+++ backend/csv_import_script.py
@@ -4,11 +4,11 @@ import os
 from foodgram.settings import BASE_DIR
 from base.models import Ingredient
 
-path = os.path.join(BASE_DIR, 'static/data/')
+path = os.path.join(BASE_DIR, 'data/')
 os.chdir(path)
 
 # Скрипт для испорта из файла CSV в базу данных Ingredient
-with open('users.csv', mode="r", encoding="utf-8") as file:
+with open('ingredients.csv', mode="r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         db = Ingredient(
