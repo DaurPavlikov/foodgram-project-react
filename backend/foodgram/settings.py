9d629f98645ccd2590d59b3a88a3650fbd25cc5f
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-04 19:17:20 +0300

    fix pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -132,8 +132,6 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.LimitPagination',
-    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
