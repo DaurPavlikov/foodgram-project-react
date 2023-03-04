f99d2f94307d928fe371b961932eeda9e100bcc6
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-04 21:28:00 +0300

    rebuild6

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -149,10 +149,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = ['http://localhost:3000',]
 CORS_URLS_REGEX = r'^/api/.*$'
 
 # Static files (CSS, JavaScript, Images)
