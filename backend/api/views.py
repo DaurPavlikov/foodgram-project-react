675001ed6aefe34a7692fe3dfe63323bda2b8bd6
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-05 00:49:35 +0300

    build3

--- backend/api/views.py
+++ backend/api/views.py
@@ -228,7 +228,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
                 'ingredients__name',
                 'ingredients__measurement_unit'
             ).annotate(amount=Sum('recipe__amount')).order_by())
-        page.setFont('Vera', 14)
+        page.setFont('DejaVu Sans', 14)
         if shopping_cart:
             indent = 20
             page.drawString(x_position, y_position, 'Cписок покупок:')
@@ -246,7 +246,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
             buffer.seek(0)
             return FileResponse(
                 buffer, as_attachment=True, filename=FILENAME)
-        page.setFont('Vera', 24)
+        page.setFont('DejaVu Sans', 24)
         page.drawString(
             x_position,
             y_position,
--- backend/recipes/management/commands/import_tags.py
+++ backend/recipes/management/commands/import_tags.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
-            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'}]
+            {'id': 0, 'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
+            {'id': 1, 'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
+            {'id': 2, 'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'}]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Тэги успешно импортированы.'))
