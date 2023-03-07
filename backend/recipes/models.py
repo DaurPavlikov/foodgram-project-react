a763aaa32781e8db4e76a30673e0594304fca8b4
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-07 15:48:42 +0300

    fatal rebuild3

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 
-from .models import RecipesRelatedManager
+from .managers import RecipesRelatedManager
 
 MIN_COOKING_TIME = 1
 MIN_INGREDIENT_AMT = 1
