585e2bd1d876c6d8e3ae99e4b6dca6d603c399ba
Даур Павликов <d-pavlikoff@yandex.ru>
2023-03-13 10:25:22 +0300

    unit

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -65,7 +65,7 @@ class TagAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measurement_unit',)
     search_fields = ('name', 'measurement_unit',)
-    list_filter = ('name',)
+    list_filter = ('measurement_unit',)
     empty_value_display = EMPTY_DISPLAY
 
 
